10f5b15d7d810ef273d51e8d3f6775c638759bd6
MarceloCamachh <marcelocamachogutierrez3@gmail.com>
2025-11-06 01:03:48 -0400

    first commit

---
+++ src/main/java/com/example/tecnoWebEmail/TecnoWebEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.tecnoWebEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnoWebEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnoWebEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tecnoWebEmail/TecnoWebEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tecnoWebEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnoWebEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
